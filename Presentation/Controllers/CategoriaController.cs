3f205a9824b29432b2a61b8769c5e84682fb2024
Diego B <u201616874@upc.edu.pe>
2019-05-04 07:53:08 -0500

    categoriacontroller

---
+++ Presentation/Controllers/CategoriaController.cs
@@ -0,0 +1,62 @@
+﻿using Business;
+using Business.Implementacion;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        IServicioCategoria servicioCategoria = new ServicioCategoria();
+
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            return View(servicioCategoria.GetAll());
+        }
+
+        public ActionResult Create()
+        {
+            Categoria categoria = new Categoria();
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            if (servicioCategoria.Insert(categoria))
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int? id) {
+            return View(servicioCategoria.FindById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (servicioCategoria.Update(categoria))
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int? id)
+        {
+            if (servicioCategoria.Delete(id.Value))
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}

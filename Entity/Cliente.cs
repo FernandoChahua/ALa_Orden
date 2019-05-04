72fa548c7bb207ce3ee03bfb6bc8880392f6022f
Diego B <u201616874@upc.edu.pe>
2019-05-03 19:28:51 -0500

    restaurar cliente

---
+++ Entity/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entity
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+
+        [Required(ErrorMessage = "Ingrese nombre de usuario")]
+        [DisplayName("Nombre de Usuario")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Ingrese contraseña")]
+        [DisplayName("Contraseña")]
+        [DataType(DataType.Password)]
+        public string Contrasena { get; set; }
+
+        [Required(ErrorMessage ="Ingrese email valido")]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Por favor ingrese un email valido")]
+        public string Email { get; set; }
+    }
+}
--- Entity/Tarjeta.cs
+++ Entity/Tarjeta.cs
@@ -5,11 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+
 namespace Entity
 {
     public class Tarjeta
     {
+        public int IdTarjeta { get; set; }
+
+        [Required(ErrorMessage = "Porfavor seleccione un cliente")]
+        public Cliente Cliente { get; set; }
+
+        [Required(ErrorMessage = "Porfavor ingrese su numero de cuenta")]
+        [DisplayName("Nro. de Cuenta")]
+        [StringLength(16, MinimumLength = 16, ErrorMessage = "La longitud del numero de cuenta es de 16 digitos")]
         public string NroCuenta { get; set; }
 
+        [Required(ErrorMessage = "Porfavor ingrese el nombre del titular")]
+        [DisplayName("Nombre del titular")]
+        public string Titular { get; set; }
+
+        [Required(ErrorMessage = "Porfavor ingrese la fecha de expiracion de la tarjeta")]
+        [DisplayName("Fecha de expiracion")]
+        public string FechaExp { get; set; }
     }
 }

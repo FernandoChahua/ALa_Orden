fb3306b236bf06d5065c4d92682ebff2b7935c7b
Fernando <fernandochahua1@gmail.com>
2019-05-03 19:03:28 -0500

    Se creo repositorio CRUD

---
+++ Data/IRepositorioCRUD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public interface IRepositorioCRUD<T>
+    {
+        bool Insert(T t);
+        bool Update(T t);
+        bool Delete(int id);
+        T FindById(int? id);
+        List<T> GetAll();
+    }
+}
+

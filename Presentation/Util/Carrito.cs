ce27099a21746d051f8bf8634935d982743dbece
Fernando <fernandochahua1@gmail.com>
2019-05-04 04:39:35 -0500

    Se creo Venta controller y la clase carrito

---
+++ Presentation/Util/Carrito.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Entity;
+
+namespace Presentation.Util
+{
+    public class Carrito
+    {
+        public List<DetallePedido> DetallePedidos { get; set; }
+
+
+        public void AumentarCantidad(DetallePedido dp)
+        {
+            foreach (DetallePedido dep in DetallePedidos)
+            {
+                if (dep.Producto.IdProducto == dp.Producto.IdProducto)
+                {
+                    dep.Cantidad++;
+                }
+            }
+        }
+        public bool Existe(DetallePedido dp)
+        {
+            foreach(DetallePedido dep in DetallePedidos)
+            {
+                if(dep.Producto.IdProducto == dp.Producto.IdProducto)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void DisminuirCantidad(DetallePedido dp)
+        {
+            foreach (DetallePedido dep in DetallePedidos)
+            {
+                if (dep.Producto.IdProducto == dp.Producto.IdProducto)
+                {
+                    dep.Cantidad++;
+                }
+            }
+        }
+
+        public void DeleteDetalle(DetallePedido dp)
+        {
+            DetallePedidos.Remove(dp);
+        }
+
+        public void AgregarDetalle(DetallePedido dp)
+        {
+            if (!Existe(dp))
+            {
+                DetallePedidos.Add(dp);
+            }
+        }
+    }
+}

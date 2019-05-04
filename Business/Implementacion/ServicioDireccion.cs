1fbf2d3315b5156f58ae1e5c526f54ce9f7be554
thealexiasilva <adsg999@gmail.com>
2019-05-04 04:26:11 -0500

    Se implementaron Iservicios de entidades restantes

---
+++ Business/Implementacion/ServicioDireccion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Implementacion;
+using Entity;
+
+namespace Business.Implementacion
+{
+    class ServicioDireccion
+    {
+    }
+}
